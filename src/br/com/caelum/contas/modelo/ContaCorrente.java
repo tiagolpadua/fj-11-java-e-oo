74f22f1420b38dc99b9fcd17d5c1a2739d38e2c4
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-02-14 10:50:42 -0300

    ex 11.5

--- src/br/com/caelum/contas/modelo/ContaCorrente.java
+++ src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -1,13 +1,18 @@
 package br.com.caelum.contas.modelo;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
-	@Override
-	public void saca(double valor) {
-		this.saldo -= (valor + 0.10);
-	}
+    @Override
+    public void saca(double valor) {
+        this.saldo -= (valor + 0.10);
+    }
 
-	public String getTipo() {
-		return "Conta Corrente";
-	}
+    public String getTipo() {
+        return "Conta Corrente";
+    }
+
+    @Override
+    public double getValorImposto() {
+        return this.getSaldo() * 0.01;
+    }
 }
